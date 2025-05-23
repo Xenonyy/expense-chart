c55d825e1d51b830107493faf47233f15eade3ab
Xenonyy <gonda.armand@gmail.com>
2025-05-23 22:13:45 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,17 @@ import data from './api/data.json';
 export const App = () => {
   return (
     <Container className="bg-[#f7e8db]">
-      <Box className="bg-[#ec755d] h-24 rounded-2xl p-5 min-w-xs md:w-lg min-lg:min-w-xl">
+      <Box className="bg-[#ec755d] h-24 rounded-2xl p-5 min-w-xs max-w-lg md:w-lg">
         <Box className="w-full h-full justify-start items-start flex-col">
           <Text className="text-white text-sm mt-[-2px] pb-1" text="My balance" />
           <Text className="text-white text-2xl font-medium" text="$921.48" />
         </Box>
         <img src={'logo.svg'} alt="logo" className="pr-4 h-18 w-18"></img>
       </Box>
-      <Box className="bg-white h-full rounded-2xl p-4 sm:p-8 mt-5 min-w-xs md:w-lg min-lg:min-w-xl ">
-        <Box className="w-full h-full justify-start items-start flex-col">
+      <Box className="bg-white h-full rounded-2xl p-4 sm:p-8 mt-5 min-w-xs max-w-lg md:w-lg ">
+        <Box className="justify-start items-start flex-col">
           <Text className="text-[#2e1e12] text-xl sm:text-3xl font-semibold" text="Spending - Last 7 days" />
-          <Box className="w-full h-full justify-evenly items-start flex-col">
+          <Box className="justify-evenly items-start flex-col">
             <Graph data={data} />
             <div className="w-full h-1 bg-[#f7f2ea] my-10" />
           </Box>
