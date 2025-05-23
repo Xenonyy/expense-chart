cfba673c6b8b539899f2f9133f3c535128a9661c
Xenonyy <gonda.armand@gmail.com>
2025-05-23 21:47:17 +0200

    width test

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ import data from './api/data.json';
 export const App = () => {
   return (
     <Container className="bg-[#f7e8db]">
-      <Box className="bg-[#ec755d] h-24 rounded-2xl p-5 min-w-xs sm:w-md md:w-lg ">
+      <Box className="bg-[#ec755d] h-24 rounded-2xl p-5 min-w-xs min-md:w-lg ">
         <Box className="w-full h-full justify-start items-start flex-col">
           <Text className="text-white text-sm mt-[-2px] pb-1" text="My balance" />
           <Text className="text-white text-2xl font-medium" text="$921.48" />
         </Box>
         <img src={'logo.svg'} alt="logo" className="pr-4 h-18 w-18"></img>
       </Box>
-      <Box className="bg-white h-full rounded-2xl p-4 sm:p-8 mt-5 min-w-xs sm:w-md md:w-lg ">
+      <Box className="bg-white h-full rounded-2xl p-4 sm:p-8 mt-5 min-w-xs min-md:w-lg ">
         <Box className="w-full h-full justify-start items-start flex-col">
           <Text className="text-[#2e1e12] text-xl sm:text-3xl font-semibold" text="Spending - Last 7 days" />
           <Box className="w-full h-full justify-evenly items-start flex-col">
